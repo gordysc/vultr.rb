73089814aad1ddd0e4adc23346c9e550848264cd
luke gordon <luke@anewday.io>
2021-09-05 09:28:50 -1000

    test(block_storage): added assertions for delete

--- test/vultr/resources/block_storage_test.rb
+++ test/vultr/resources/block_storage_test.rb
@@ -55,6 +55,15 @@ class BlockStorageResourceTest < Minitest::Test
     body = {size_gb: 50, label: "Example Block Storage"}
     stub = stub_request("blocks/#{block_id}", method: :patch, body: body, response: {})
     client = Vultr::Client.new(api_key: "fake", adapter: :test, stubs: stub)
+    
     assert client.block_storage.update(block_id: block_id, **body)
+  end
+
+  def test_delete
+    block_id = "cb676a46-66fd-4dfb-b839-443f2e6c0b60"
+    stub = stub_request("blocks/#{block_id}", method: :delete, response: {})
+    client = Vultr::Client.new(api_key: "fake", adapter: :test, stubs: stub)
+    
+    assert client.block_storage.delete(block_id: block_id)
   end
 end
